54a3608c879149016169a7850b0645be553ef926
Daniel Azuma <dazuma@gmail.com>
2009-11-19 17:37:36 -0800

    Version 0.2.3

--- lib/versionomy/version.rb
+++ lib/versionomy/version.rb
@@ -37,7 +37,7 @@
 module Versionomy
   
   # Current gem version, as a frozen string.
-  VERSION_STRING = '0.2.2'.freeze
+  VERSION_STRING = '0.2.3'.freeze
   
   # Current gem version, as a Versionomy::Value.
   VERSION = ::Versionomy.parse(VERSION_STRING, :standard)
